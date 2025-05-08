745348f7c1ecc5f368133fe9a8a6245eb5c4043a
mirdukkkkk <69540930+mirdukkkkk@users.noreply.github.com>
2025-05-09 02:05:22 +1200

    better generate script

--- generate.sh
+++ generate.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 set -e
 
+rm -rf generated/*
+
 mkdir -p generated/ts
-
-find proto -type f -name '*.proto' | while read -r proto; do
-  relpath="${proto#proto/}"
-  basename="${relpath%.proto}"
-  out_js="generated/ts/$basename.js"
-  out_dts="generated/ts/$basename.d.ts"
-
-  mkdir -p "$(dirname "$out_js")"
-
-  echo "Генерация $out_js и $out_dts"
-
-  pbjs -t static-module -o "$out_js" "$proto"
-  pbts -o "$out_dts" "$out_js"
-done
+pbjs -t static-module -w commonjs -o generated/ts/index.js proto/*.proto
+pbts -o generated/ts/index.d.ts generated/ts/index.js
